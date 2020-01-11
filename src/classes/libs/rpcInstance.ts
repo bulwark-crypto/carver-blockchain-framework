94da87b2a0de44f92a1db9ded59aad48fb2102e4
Hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2020-01-11 14:44:10 -0500

    Added rpc queue

--- src/classes/libs/rpcInstance.ts
+++ src/classes/libs/rpcInstance.ts
@@ -5,6 +5,9 @@ const createRpcInstance = () => {
     const rpcLib = require('node-bitcoin-rpc')
     rpcLib.init(config.rpc.host, config.rpc.port, config.rpc.username, config.rpc.password);
 
+    let callsQueue: any[] = []
+    let processing = false;
+
     return {
         call: (fn: string, params: any[] = []) => {
             if (!fn) {
@@ -15,8 +18,17 @@ const createRpcInstance = () => {
                 params = [];
             }
 
-            return new Promise((resolve, reject) => {
+            const callNext = () => {
+                if (callsQueue.length === 0) {
+                    return;
+                }
+
+                const { fn, params, resolve, reject } = callsQueue.shift();
+
+                processing = true;
                 rpcLib.call(fn, params, (err: any, data: any) => {
+                    processing = false;
+
                     if (err || !!data.error) {
                         console.log('Call failed', fn, err || data.error);
 
@@ -24,17 +36,40 @@ const createRpcInstance = () => {
                         const errorResponse = typeof errorResult === 'object' ? JSON.stringify(errorResult) : errorResult;
 
                         reject(new Error(errorResponse));
-                        return;
+                    } else {
+                        resolve(data.result);
                     }
 
-                    resolve(data.result);
+                    callNext();
                 });
+            }
+
+            const addCallToQueue = (resolve: any, reject: any) => {
+                const queueItem = { fn, params, resolve, reject }
+
+                callsQueue.push(queueItem);
+                if (processing) {
+                    return;
+                }
+
+                callNext();
+
+            }
+
+            return new Promise((resolve, reject) => {
+                addCallToQueue(resolve, reject);
             });
         }
     }
 }
+
+
+const globalInstance = createRpcInstance()
+const useGlobalInstance = () => {
+    return globalInstance;
+}
 //const rpc = createRpcInstance()
 
 export {
-    createRpcInstance
+    createRpcInstance: useGlobalInstance
 }
